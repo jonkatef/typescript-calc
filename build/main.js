7ae2386e288624d07b5d8ec67b32fa1e97826b8d
yoni hadar <jonkatef@gmail.com>
2023-06-06 12:19:05 +0200

    first commit

---
+++ build/main.js
@@ -0,0 +1,29 @@
+// Variable
+var a;
+// a = "hello" ERROR
+// a = false  ERROR
+a = 200;
+var b;
+b = 200;
+// b = false // error
+b = "true";
+b = new Date();
+var c;
+c = true;
+c = "hello";
+c = 145;
+c = new Date();
+var f;
+f = "hello";
+f = 250;
+// ------------------ F U N C T I O N S ------------------------------------------------------\
+function func(age, name) {
+    console.log(name + " is " + age + " years old");
+}
+func(34, "Erez");
+function fun2() {
+    console.log("another function");
+}
+function func3() {
+    console.log("My third function");
+}
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+// Variable
+
+let a: number;
+// a = "hello" ERROR
+// a = false  ERROR
+a = 200;
+
+let b: number | string | Date;
+b = 200;
+// b = false // error
+b = "true";
+b = new Date();
+
+let c: any;
+c = true;
+c = "hello";
+c = 145;
+c = new Date();
+
+let f;
+f = "hello";
+f = 250;
+
+// ------------------ F U N C T I O N S ------------------------------------------------------\
+function func(age: number, name: string) {
+  console.log(name + " is " + age + " years old");
+}
+
+func(34, "Erez");
+
+function fun2() {
+  console.log("another function");
+}
+
+function func3() {
+  console.log("My third function");
+}
